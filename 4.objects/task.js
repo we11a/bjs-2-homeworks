ffc3675ed12ac683bce4ec5788f306a34241c32e
Valentina Volman <vavolman@gmail.com>
2021-10-30 13:36:35 +0300

    Task 4 is completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+    this.marks = [];
+    this.marks.push(mark);
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  this.marks = mark;
+}
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((sum, item, idx, arr) => {
+    sum += item;
+    if (idx === arr.length - 1) {
+      return sum / arr.length;
+    } else {
+      return sum;
+    }
+  })
+}
+
+
+Student.prototype.exclude = function(reason) {
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
+}
